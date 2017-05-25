b1d28ba07f0b6150f1ca65a8ee9ed6cc6129b526
Charlotte Moore <mooc123@gmail.com>
2017-05-24 23:14:35 -0600

    added model tests

--- spec/models/howler_spec.rb
+++ spec/models/howler_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Howler, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'relationships' do
+    it { should belong_to(:user) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:title)}
+    it { should validate_presence_of(:text)}
+  end
 end
