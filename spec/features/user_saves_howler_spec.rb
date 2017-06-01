bdb81820ce466f8b470994952e1ac0c9228f6170
Charlotte Moore <mooc123@gmail.com>
2017-05-31 21:35:52 -0600

    tested clear button, stupid cors issue

--- spec/features/user_saves_howler_spec.rb
+++ spec/features/user_saves_howler_spec.rb
@@ -1,9 +1,28 @@
 require 'rails_helper'
 
-describe "after user submits text and clicks save" do
+describe "after user submits text and clicks save", js: true do
   it "the data is persisted in database" do
-    # need to add title after james PR merged
+    VCR.use_cassette('services/save-howler') do
+      user = User.create(name: 'name')
 
-    # this is going to be in integration test with selenium when we get it working
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      expect(page).to_not have_button('Sign in with Twitter To Save')
+
+      find("textarea[name='text']").set('I like things')
+      find("input[name='howler-title']").set('title yo')
+      find('input[name="get-data"]').click
+
+      expect(page).to have_button('Save')
+
+      click_on('Save')
+      sleep(10)
+
+      # expect(user.howlers.count).to eq(1)
+
+      # expect(current_path).to eq(howlers_path)
+    end
   end
 end
